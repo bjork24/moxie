d9a0eeea1944563239b47305abd343cbe64cdd92
bjork24 <dan.chilton@gmail.com>
2014-12-08 12:16:54 -0600

    Removign hash bang

--- src/js/app.js
+++ src/js/app.js
@@ -22,7 +22,7 @@ Moxie = (function($){
     page('/phlog', controller.phlog.index);
     page('/phlog/:id', controller.phlog.entry);
     page('*', controller.notFound);
-    page({ hashbang: true });
+    page();
   };
 
   // private controller method
